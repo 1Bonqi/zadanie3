7b695a9ed8497eb1085e531254d294b9c8346ce9
Vlad <v-kucher@bk.ru>
2024-09-01 11:16:26 +0300

    Add files via upload

---
+++ module_5_4.py
@@ -0,0 +1,68 @@
+class house:
+    House_history = []
+
+    def __new__(cls, *args, **kwargs):
+        cls.House_history.append(args[0])
+        return object.__new__(cls)
+
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        if new_floor < 1 or new_floor > self.number_of_floors:
+            print('Такого этажа не существует')
+        else:
+            for i in range(1, new_floor + 1):
+                print(i)
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f'Название:{self.name},колличество этажей:{self.number_of_floors}'
+
+    def __eq__(self, other):
+        return self.number_of_floors == other.number_of_floors
+
+    def __lt__(self, other):
+        return self.number_of_floors > other.number_of_floors
+
+    def __gt__(self, other):
+        return self.number_of_floors > other.number_of_floors
+
+    def __le__(self, other):
+        return self.number_of_floors <= other.number_of_floors
+
+    def __ge__(self, other):
+        return self.number_of_floors >= other.number_of_floors
+
+    def __ne__(self, other):
+        return self.number_of_floors != other.number_of_floors
+
+    def __add__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors += value
+            return self
+
+    def __radd__(self, value):
+        self.number_of_floors += value
+        return self
+
+    def __iadd__(self, value):
+        self.number_of_floors += value
+        return self
+
+    def __del__(self):
+        print(f'"Название:{self.name},снесен,но останется в истории')
+
+
+h1 = house('ЖК Эльбрус', 10)
+print(house.House_history)
+h2 = house('ЖК Акация', 20)
+print(house.House_history)
+h3 = house('ЖК Матрёшки', 20)
+print(house.House_history)
+del h2
+del h3
+print(house.House_history)
